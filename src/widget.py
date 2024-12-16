363cf31232181f03d4b03e706fe051c45a5708c4
Mikhail Sol <solomennikov_m@mail.ru>
2024-12-16 22:17:08 +0300

    add docstrings

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
 def mask_account_card(card_type_number: str) -> str:
+    """Возвращает строку с замаскированным номером"""
     list_card = card_type_number.rsplit(maxsplit=1)
     if "Счет" in card_type_number:
         return f"{list_card[0]} **{list_card[1][-4:]}"
@@ -7,4 +8,6 @@ def mask_account_card(card_type_number: str) -> str:
 
 
 def get_date(date: str) -> str:
-    pass
+    """возвращает строку с датой в формате 'ДД.ММ.ГГГГ'"""
+    list_date = date.split("-")
+    return f"{list_date[2][:2]}.{list_date[1]}.{list_date[0]}"
