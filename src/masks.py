900313ed6fdaabf37004e38d7c3acede4ac41dbb
Mikhail Sol <solomennikov_m@mail.ru>
2024-12-17 14:55:18 +0300

    update widget

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,10 @@
-def get_mask_card_number(card_number: int) -> str:
+def get_mask_card_number(card_number: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску."""
     card_mask = [i for i in str(card_number)]
-    return f"{"".join(card_mask[:4])} {"".join(card_mask[4:6])}** **** {"".join(card_mask[12:])}"
+    return f"{"".join(card_mask[:4])} {"".join(card_mask[4:6])}** **** {"".join(card_mask[-4:])}"
 
 
-def get_mask_account(account_number: int) -> str:
+def get_mask_account(account_number: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску."""
     mask_account = [i for i in str(account_number)]
-    return f"**{"".join(mask_account[16:])}"
+    return f"**{"".join(mask_account[-4:])}"
--- src/widget.py
+++ src/widget.py
@@ -1,10 +1,13 @@
+from masks import get_mask_account, get_mask_card_number
+
+
 def mask_account_card(card_type_number: str) -> str:
     """Возвращает строку с замаскированным номером"""
     list_card = card_type_number.rsplit(maxsplit=1)
     if "Счет" in card_type_number:
-        return f"{list_card[0]} **{list_card[1][-4:]}"
+        return f"{list_card[0]} {get_mask_account(list_card[1])}"
     else:
-        return f"{list_card[0]} {list_card[1][:4]} {list_card[1][4:6]}** **** {list_card[1][-4:]}"
+        return f"{list_card[0]} {get_mask_card_number(list_card[1])}"
 
 
 def get_date(date: str) -> str:
