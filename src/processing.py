96436aa52b86e4a5d9f68b53f8525de52fa12e5b
Mikhail Sol <solomennikov_m@mail.ru>
2024-12-25 22:29:09 +0500

    add func sort by date

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,11 @@
-def filter_by_state(list_dict: list, state: str = "EXECUTED") -> list:
+def filter_by_state(list_dict: list[dict], state: str = "EXECUTED") -> list[dict]:
     new_list_dict = []
     for i in list_dict:
         for value in i.values():
             if value == state:
                 new_list_dict.append(i)
     return new_list_dict
+
+def sort_by_date(list_dict: list[dict], data: bool = True ) -> list[dict]:
+    sort_list_by_data = sorted(list_dict, key=lambda x: x['date'],reverse = data)
+    return sort_list_by_data
