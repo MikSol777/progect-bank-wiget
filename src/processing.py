c4c4753d443f2ca71c1ae949b877c2766dea56e2
Mikhail Sol <solomennikov_m@mail.ru>
2024-12-25 22:33:30 +0500

    update processing.py

--- src/processing.py
+++ src/processing.py
@@ -6,6 +6,7 @@ def filter_by_state(list_dict: list[dict], state: str = "EXECUTED") -> list[dict
                 new_list_dict.append(i)
     return new_list_dict
 
-def sort_by_date(list_dict: list[dict], data: bool = True ) -> list[dict]:
-    sort_list_by_data = sorted(list_dict, key=lambda x: x['date'],reverse = data)
+
+def sort_by_date(list_dict: list[dict], data: bool = True) -> list[dict]:
+    sort_list_by_data = sorted(list_dict, key=lambda x: x["date"], reverse=data)
     return sort_list_by_data
