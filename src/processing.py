5dfa5beba09ebc1840c487b157e75d22dc1e3c2a
Mikhail Sol <solomennikov_m@mail.ru>
2024-12-27 16:20:34 +0500

    update

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,8 @@
 def filter_by_state(list_dict: list[dict], state: str = "EXECUTED") -> list[dict]:
-    """возвращает новый список словарей, содержащий только те словари, у которых ключ state соответствует указанному значению"""
-    return [i for i in list_dict if i["state"] == state]
+    """Возвращает новый список словарей, содержащий только те словари, у которых ключ state соответствует указанному значению"""
+    return [i for i in list_dict if i.get("state") == state]
 
 
 def sort_by_date(list_dict: list[dict], sort_date: bool = True) -> list[dict]:
-    """возвращает новый список, отсортированный по дате (date)"""
+    """Возвращает новый список, отсортированный по дате (date)"""
     return sorted(list_dict, key=lambda x: x["date"], reverse=sort_date)
