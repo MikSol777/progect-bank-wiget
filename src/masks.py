35f41cbdcdbe35e5721ad9f2a9881a0ac4170713
Mikhail Sol <solomennikov_m@mail.ru>
2025-01-12 19:13:34 +0500

    update

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,14 @@
 def get_mask_card_number(card_number: str) -> str:
     """Принимает на вход номер карты и возвращает ее маску."""
+    if len(card_number) != 16 or card_number.isdigit() == False:
+        raise ValueError("Неверный ввод номера карты")
     card_mask = [i for i in str(card_number)]
     return f"{"".join(card_mask[:4])} {"".join(card_mask[4:6])}** **** {"".join(card_mask[-4:])}"
 
 
 def get_mask_account(account_number: str) -> str:
     """Принимает на вход номер счета и возвращает его маску."""
+    if len(account_number) != 20 or account_number.isdigit() == False:
+        raise ValueError("Неверный ввод номера счета")
     mask_account = [i for i in str(account_number)]
     return f"**{"".join(mask_account[-4:])}"
--- src/widget.py
+++ src/widget.py
@@ -13,4 +13,10 @@ def mask_account_card(card_type_number: str) -> str:
 def get_date(date: str) -> str:
     """Возвращает строку с датой в формате 'ДД.ММ.ГГГГ'"""
     list_date = date.split("-")
+    if len(list_date[0]) != 4 or len(list_date[1]) != 2:
+        raise ValueError("Неверный формат даты")
+    if list_date[0].isdigit() == False or list_date[1].isdigit() == False or list_date[2][0:2].isdigit() == False:
+        raise ValueError("Неверный формат даты")
+    if int(list_date[2][0:2]) > 31 or int(list_date[2][0:2]) <= 0 or int(list_date[1]) > 12 or int(list_date[1]) <=0:
+        raise ValueError("Неверный формат даты")
     return f"{list_date[2][:2]}.{list_date[1]}.{list_date[0]}"
